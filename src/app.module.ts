5bf80cf74871fec2c40796795bf52d704929d10f
mairandomness <mairakodama@gmail.com>
2021-08-05 15:36:16 -0700

    Add part of the many-to-many relationship between location and provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Connection } from 'typeorm';
 import { LocationsModule } from './locations/locations.module';
+import { ProviderReviewsModule } from './provider-reviews/provider-reviews.module';
 import { ProvidersModule } from './providers/providers.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ autoLoadEntities: true, }), ProvidersModule, LocationsModule],
+  imports: [TypeOrmModule.forRoot({ autoLoadEntities: true, }), ProvidersModule, LocationsModule, ProviderReviewsModule],
   controllers: [],
   providers: [],
 })
